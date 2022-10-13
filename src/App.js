b32d23cc035f73a9d38b688867e106170edfec95
mjunaidali98 <mjunaidali98@gmail.com>
2022-10-13 18:41:28 +0500

    builder integration

--- src/App.js
+++ src/App.js
@@ -1,34 +1,93 @@
-import React, { useState, useEffect } from "react"
-import Home from "./pages/home";
+// import React, { useState, useEffect } from "react"
+// import Home from "./pages/home";
+// import About from "./pages/about";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import NotFound from "./pages/404"
+// import { BuilderComponent, builder } from '@builder.io/react';
+// builder.init('9f450fb622bf4a49b9286042970bcf4a')
+
+// function App() {
+//   const [builderContentJson, setBuilderContentJson] = useState(null)
+
+//   useEffect(() => {
+//     if (typeof window !== "undefined") {
+//       builder.get('page', { url: window.location.pathname })
+//         .promise().then(setBuilderContentJson)
+//     }
+//     // return 
+//   }, [])
+// return (
+//   <div className="bg-[#F6F6F6] ">
+//     <Router>
+//       <Routes>
+//         <Route path="/home" element={<Home />} exact />
+//         <Route path="/" element={<Home />} exact />
+//         <Route path="/about" element={<About />} exact />
+//         <Route path="/shop" element={<BuilderComponent model="page" content={builderContentJson} />} exact />
+
+//       </Routes>
+//     </Router>
+//   </div>
+// );
+// }
+
+// export default App;
+
+
+
+import { useEffect, useState } from "react";
+import { BuilderComponent, builder, useIsPreviewing } from "@builder.io/react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
 import About from "./pages/about";
-import { BuilderComponent, builder } from '@builder.io/react';
-builder.init('9f450fb622bf4a49b9286042970bcf4a')
+// Put your API key here
+builder.init("19a078a546e8451b8f93e16018bc04da");
 
-function App() {
-  const [builderContentJson, setBuilderContentJson] = useState(null)
+// set whether you're using the Visual Editor,
+// whether there are changes,
+// and render the content if found
+export default function CatchAllRoute() {
+  const isPreviewingInBuilder = useIsPreviewing();
+  const [notFound, setNotFound] = useState(false);
+  const [content, setContent] = useState(null);
 
+  // get the page content from Builder
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      builder.get('page', { url: window.location.pathname })
-        .promise().then(setBuilderContentJson)
+    async function fetchContent() {
+      const content = await builder
+        .get("homepage", {
+          url: window.location.pathname
+        })
+        .promise();
+      console.log(content)
+      setContent(content);
+      setNotFound(!content);
     }
+    fetchContent();
+  }, [window.location.pathname]);
 
-    // return 
-  }, [])
+  // if no page is found, return a 404 page
+  if (notFound && !isPreviewingInBuilder) {
+    return <div>Sorry! Page not found</div>
+  }
+
+  // return the page when found
   return (
-    <div className="bg-[#F6F6F6] ">
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Home />} exact />
-          <Route path="/" element={<Home />} exact />
-          <Route path="/about" element={<About />} exact />
-          <Route path="/shop" element={<BuilderComponent model="page" content={builderContentJson} />} exact />
-
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <head>
+        <title>{content?.data.title}</title>
+      </head>
+      {/* Render the Builder page */}
+      <div className="bg-[#F6F6F6] ">
+        <Router>
+          <Routes>
+            <Route path="/home" element={<Home />} exact />
+            <Route path="/" element={<Home />} exact />
+            <Route path="/about" element={<About />} exact />
+          </Routes>
+        </Router>
+      </div>
+      <BuilderComponent model="page" content={content} />
+    </>
   );
 }
-
-export default App;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,1 +1,1 @@
-export const NotFound = () => <h1>No page found for this URL, did you publish it?</h1>;
+export default function NotFound  (){ <h1>No page found for this URL, did you publish it?</h1>};
